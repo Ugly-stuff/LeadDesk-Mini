58607a742a7c213fc7f17cb3313727b97a06ad9c
Ugly-stuff <souvikpramanik232@gmail.com>
2026-07-26 02:11:11 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
   res.send("LeadDesk API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
